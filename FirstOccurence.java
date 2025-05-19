17cea6a8f80595eaea3b7f8638d7eb4290dddb03
Kunj Maheshwari <kunjm@outlook.com>
2025-05-19 21:11:48 +0530

    Added First Occurence

---
+++ FirstOccurence.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class FirstOccurence{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence");
+        String str = sc.nextLine();
+
+        if(!str.matches("[a-zA-Z]")){
+            System.out.println("Invalid input");
+            return;
+        }
+
+        System.out.println("Enter the character to be searched");
+        String c = sc.nextLine();
+
+        System.out.println("Enter the character to be replaced");
+        String d = sc.nextLine();
+
+        if(!d.matches("[a-zA-Z]+")){
+            System.out.println("Inavlid character Input");
+            return;
+        }
+
+        if(str.contains(c)){
+            str = str.replaceFirst(c, d);
+            System.out.println(str);
+        }else{
+            System.out.println("Character is not found in the String");
+        }
+    }
+}
